c1f7b0a5c091b336b29eabf851f6df03dbcb4b23
Shiva Acharjee <shivaacharjeesavp@gmail.com>
2017-10-17 10:44:37 +0530

    initial commit

---
+++ spec/source-def-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SourceDefView from '../lib/source-def-view';
+
+describe('SourceDefView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
